69756d3d2e48fb65f2348662df0ccee0b61712b3
martinjamesl <mjl.drst@gmail.com>
2022-06-12 23:34:00 +0700

    feat: challenge-07-martinjamesl commit

--- promise.js
+++ promise.js
@@ -1,7 +1,16 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const arrEmosi = (await promiseTheaterIXX()).concat(
+      await promiseTheaterVGC()
+    );
+    return arrEmosi.filter((element) => element.hasil === emosi).length;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
